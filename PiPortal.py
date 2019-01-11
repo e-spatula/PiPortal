dcc8ee783ef04e5e90454b9de041f7828bd65280
Eddie Atkinson <edwardgordonatkinson@gmail.com>
2019-01-11 10:59:46 +0800

    adds uidSplitter method

--- PiPortal.py
+++ PiPortal.py
@@ -11,3 +11,22 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 cardURL = "https://auth.uwamakekers.com/api/card"
 token = config["setup"]["token"]
+
+"""
+Method that takes the uid string provided by the ESP, strips it of the 
+extra formatting added by the ESP and converts the hexadecimal string to an array 
+of 4 digits. 
+
+For example the uid string b'04050607' becomes:
+0405060708
+which becomes:
+[4,5,6,7]
+"""
+def uidSplitter(uid):
+    uid = str(uid)
+    uid = uid.replace("b'", "").replace("'", "")
+    byteArray = []
+    for i in range(1,8,2):
+        byte = int(uid[i-1 : i+1], 16)
+        byteArray.append(byte)
+    return(byteArray)
